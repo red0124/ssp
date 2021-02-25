f4430fdb7704d787fc87636b11234b297e1a4349
ado <adnan.abd0124@gmail.com>
2021-02-25 21:48:52 +0100

    try icc win ci

--- script/ci_win_setup_icc.bat
+++ script/ci_win_setup_icc.bat
@@ -4,3 +4,4 @@ REM SPDX-License-Identifier: MIT
 
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+@call "C:\Program Files (x86)\Intel\oneAPI\tbb\%LATEST_VERSION%\env\vars.bat"
