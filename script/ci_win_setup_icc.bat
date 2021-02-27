762d370d817bcb2624815344b3a4a400a6beffe5
ado <adnan.abd0124@gmail.com>
2021-02-27 13:55:27 +0100

    remove win ci icc scripts

--- script/ci_win_setup_icc.bat
+++
@@ -1,7 +0,0 @@
-REM SPDX-FileCopyrightText: 2020 Intel Corporation
-REM
-REM SPDX-License-Identifier: MIT
-
-for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
-@call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
-@call "C:\Program Files (x86)\Intel\oneAPI\tbb\%LATEST_VERSION%\env\vars.bat"
