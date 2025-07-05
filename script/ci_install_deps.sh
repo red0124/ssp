c708d93db30a092ed56a501ea305f025abd60347
red0124 <75804778+red0124@users.noreply.github.com>
2025-07-05 15:17:58 +0200

    CI Update (#53)
    * Remove 32-bit CI workflows
    
    * [skip ci] Update Windows CI images
    
    * [skip ci] Update MacOS CI image
    
    * [skip ci] Remove icc scripts
    
    * Update README

--- script/ci_install_deps.sh
+++ script/ci_install_deps.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 JOBS=4
 BUILD_TYPE=Debug
 
-set -eux
+set -ex
 
 git clone https://github.com/red0124/doctest -b master --depth 1
 
@@ -12,6 +12,10 @@ cmake -S doctest -B doctest/build \
     -D DOCTEST_WITH_MAIN_IN_STATIC_LIB=NO \
     -D DOCTEST_WITH_TESTS=NO
 
-cmake --build doctest/build --config ${BUILD_TYPE} --target install -j ${JOBS}
+if [[ "${1}" == "sudo" ]]; then
+    sudo cmake --build doctest/build --config ${BUILD_TYPE} --target install -j ${JOBS}
+else
+    cmake --build doctest/build --config ${BUILD_TYPE} --target install -j ${JOBS}
+fi
 
 rm -rf doctest
